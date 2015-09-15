6313c853f279e8b53f61e3c943bc4a4a5365e638
Student <student@RU-001.local>
2015-09-15 16:12:03 -0600

    API

--- main.js
+++ main.js
@@ -1,56 +1,59 @@
-angular.module("app", [])
+// angular.module("app", [])
 
-var mainControllerFunc = function($scope){
+// var mainControllerFunc = function($scope){
 
-	$scope.markers = []
+// 	$scope.markers = []
 	
-	$scope.addMarker = function(event){
-		$scope.markerBox = {
-			xposition: (event.pageX - 24),
-			yposition: (event.pageY - 24),
-			comment: prompt('enter a comment'),
-			commentVisibility: false,
-		}
-		$scope.markers.push($scope.markerBox)
-		console.log($scope.markers)
+// 	$scope.addMarker = function(event){
+// 		$scope.markerBox = {
+// 			xposition: (event.pageX - 24),
+// 			yposition: (event.pageY - 24),
+// 			//comment: prompt('enter a comment'),
+// 			commentVisibility: false,
+// 		}
+// 		$scope.markers.push($scope.markerBox)
+// 		console.log($scope.markers)
 		
-	}
+// 	}
 	
-	$scope.removeMarker = function(index){
-		$scope.markers.splice(index,1)
-	}
+// 	$scope.removeMarker = function(index){
+// 		$scope.markers.splice(index,1) 
+// 	}
 
-	$scope.togleComment = function(index) {
-		$scope.markers[index].commentVisibility = !$scope.markers[index].commentVisibility
-	}	
+// 	$scope.toggleComment = function(index) {
+// 		$scope.markers[index].commentVisibility = !$scope.markers[index].commentVisibility
+// 	}	
+// }
+
+// angular.module("app").controller("mainController", ["$scope", mainControllerFunc])
+
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  	center: {lat:-16.5004126, lng: -151.74149039},
+    zoom: 8
+  });
+
+
+ marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+  	posiiton: {lat:-16.5004126, lng: -151.74149039}
+  });
+  marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-angular.module("app").controller("mainController", ["$scope", mainControllerFunc])
+    //center: {lat: 40.25220729, lng: -105.8230671},
